76f4c382af43a663fb3b02e5024b11566440c01c
Tetiana Khotiaintseva <tetiana.khotiaintsev@allegrogroup.com>
2017-11-28 16:55:34 +0100

    Section 7, Lecture 86: Lifecycle hooks - update

--- section7/src/components/People/People.jsx
+++ section7/src/components/People/People.jsx
@@ -21,6 +21,23 @@ class People extends Component{
     console.log('[People.js] inside componentWillUnount')
   }
 
+  componentWillReceiveProps (nextProps) {
+    console.log('[UPDATE People.js] inside componentWillReceiveProps', nextProps)
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('[UPDATE People.js] inside shouldComponentUpdate', nextProps, nextState)
+    return nextProps.people !== this.props.people;
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+    console.log('[UPDATE People.js] inside componentWillUpdate', nextProps, nextState)
+  }
+
+  componentDidUpdate () {
+    console.log('[UPDATE People.js] inside componentDidUpdate')
+  }
+
   render = () => {
     console.log('[People.js] inside render')
     return this.props.people.map((person, index) => {
