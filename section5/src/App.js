01fa74d91cf5ddfa14b4817f56e78d1f5f4438bb
Tetiana Khotiaintseva <tetiana.khotiaintsev@allegrogroup.com>
2017-11-28 12:18:52 +0100

    Section 5, lecture 66: css modules

--- section5/src/App.js
+++ section5/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person.jsx';
 
 
@@ -63,18 +63,18 @@ class App extends Component {
         style.color = 'red';
     }
 
-    const classes = []
+    const assignedClasses = []
     if (this.state.people.length <= 2){
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if (this.state.people.length <= 1){
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Section 5: styling</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button style={style}
           onClick={this.showPeopleHandler}>{this.state.showPeople ? "Hide" : "Show"} People</button>
         {people}
--- section5/src/Person/Person.jsx
+++ section5/src/Person/Person.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={props.click}>{ props.name } is {props.age} years old.</p>
       <input onChange={props.changed} defaultValue={props.name}></input>
     </div>
