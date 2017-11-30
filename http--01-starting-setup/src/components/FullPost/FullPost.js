bea0e5510511ecfd1d3fd392b55842d2f607fecf
Tetiana Khotiaintseva <tetiana.khotiaintsev@allegrogroup.com>
2017-11-30 16:20:41 +0100

    Section 9, Lecture 152: DELETE request

--- http--01-starting-setup/src/components/FullPost/FullPost.js
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -19,6 +19,16 @@ class FullPost extends Component {
     }
   }
 
+
+  deletePostHandler = () => {
+    if (this.props.id ){
+      axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      .then(response => {
+        console.log(response)
+      });
+    }
+  }
+
   render () {
       let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
       if (this.props.id ) {
@@ -30,7 +40,7 @@ class FullPost extends Component {
                 <h1>{this.state.loadedPost.title}</h1>
                 <p>{this.state.loadedPost.body}</p>
                 <div className="Edit">
-                    <button className="Delete">Delete</button>
+                    <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                 </div>
             </div>
         );
