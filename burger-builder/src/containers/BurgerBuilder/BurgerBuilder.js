45d57abfc261cc9eb619a1aa19019e6026040f3b
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2017-12-02 20:39:52 +0100

    Section 10, Lecture 165: sending data to the server

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -37,7 +38,28 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert("you purchased")
+    // alert("you purchased");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Max',
+        address: {
+          street: 'Teststreet 1',
+          zipcode: '12345'
+        },
+        phone: '74527852'
+      },
+      deliveryMethod: 'fastest',
+    }
+    axios.post('/orders.json', order)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      });
+
   }
 
   updatePurchaseState = (ingredients) => {
