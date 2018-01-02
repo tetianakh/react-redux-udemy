97a767e3dfa7f6a647d7d121061c2b4ba845e704
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2018-01-02 22:57:37 +0100

    Section 11, Lecture 189: navigating programmatically

--- section11/src/containers/Blog/Posts/Posts.js
+++ section11/src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import axios from '../../../axios';
@@ -28,17 +27,20 @@ class Posts extends Component {
           });
   }
   postSelectedHandler = (id) => {
-      this.setState({selectedPostId: id});
+    this.props.history.push({
+      pathname: "/" + id
+    })
   }
 
   render () {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
     if (!this.state.error) {
         posts = this.state.posts.map(post => {
-            return <Link to={'/' + post.id} key={post.id}><Post
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} /></Link>;
+            return <Post
+              key={post.id}
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)} />;
         });
     }
 
